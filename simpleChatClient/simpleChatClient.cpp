3a4ba8e1c2408a5630c3fae63b0a88879c2f9699
My Name <my-name@chromium.org>
2019-07-12 18:06:16 +0800

    Initial commit.

--- simpleChatClient/simpleChatClient.cpp
+++ simpleChatClient/simpleChatClient.cpp
@@ -1,57 +1,50 @@
-// simpleChat.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// simpleChatClient.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include "pch.h"
 #include <iostream>
+
 #include <WinSock2.h>
 #include <WS2tcpip.h>
-#pragma comment (lib, "ws2_32.lib")
+#pragma comment(lib, "ws2_32.lib")  //加载 ws2_32.dll
 
-using namespace std;
-
-int main(void)
-{
-	//初始化 DLL
+int main() {
+	//初始化DLL
 	WSADATA wsaData;
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
 	//创建套接字
-	SOCKET servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	SOCKET sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-	//绑定套接字
+	//向服务器发起请求
 	sockaddr_in sockAddr;
 	memset(&sockAddr, 0, sizeof(sockAddr));  //每个字节都用0填充
-	sockAddr.sin_family = PF_INET;  //使用IPv4地址
-	int tmpRes = inet_pton(sockAddr.sin_family,"127.0.0.1",&sockAddr.sin_addr.s_addr); //inet_addr("127.0.0.1");  //具体的IP地址
+	sockAddr.sin_family = PF_INET;
+	int tmpRes = inet_pton(sockAddr.sin_family, "127.0.0.1", &sockAddr.sin_addr.s_addr); //inet_addr("127.0.0.1");  //具体的IP地址
 	if (tmpRes != 1)
 	{
-		cout << tmpRes << endl;
 		return -1;
 	}
-	sockAddr.sin_port = htons(1234);  //端口
-	bind(servSock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR));
+	sockAddr.sin_port = htons(1234);
+	connect(sock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR));
 
-	//进入监听状态
-	listen(servSock, 20);
+	//接收服务器传回的数据
+	char szBuffer[MAXBYTE] = { 0 };
+	recv(sock, szBuffer, MAXBYTE, NULL);
 
-	//接收客户端请求
-	SOCKADDR clntAddr;
-	int nSize = sizeof(SOCKADDR);
-	SOCKET clntSock = accept(servSock, (SOCKADDR*)&clntAddr, &nSize);
-
-	//向客户端发送数据
-	const char *str = "Hello World!";
-	send(clntSock, str, strlen(str) + sizeof(char), NULL);
+	//输出接收到的数据
+	printf("Message form server: %s\n", szBuffer);
 
 	//关闭套接字
-	closesocket(clntSock);
-	closesocket(servSock);
+	closesocket(sock);
 
-	//终止 DLL 的使用
+	//终止使用 DLL
 	WSACleanup();
 
+	system("pause");
 	return 0;
 }
+
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
